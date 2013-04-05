f31dbc98820a82596ec9a7ab31ff4a8beb6766d3
Moomasterq <cpsaicontact@gmail.com>
2013-04-04 22:30:23 -0400

    Added in old code for main.py
    I had the other code written out on a plane, but didn't realize that
    raw_input() was supposed to be used instead of input().  This made it
    all work, and thankfully it does.  The reason the old file is
    non-existent is because github gave me trouble.

--- main.py
+++ main.py
@@ -1,13 +1,56 @@
-#Created on 4/4/13 by Moomasterq
-#This file really does nothing
+#Imports other file(s)
+from basic import gtorl
+from math import sin, cos, tan
+from trig import csc, sec, cot
 
-#Takes input
-#This took me longer than I'd like to admit to figure out it was raw_input() not input()
-i1 = raw_input('Enter word: ')
+#First input that will act as a"guide" in a sense
+mi1 = raw_input("Enter what calculation you want to do, or type help for more options: ")
+mi2 = mi1.lower() #Supposed to make the main input lowercase
+mi = str(mi1) #Supposed to make the main input into a string
 
-#Makes angry
-def angry(x):
-    return x.upper()
+#Takes in the x and y input
+i1 = raw_input("Define your X value: ")
+i2 = raw_input("Define your Y value: ")
 
-#Prints input 1
-print i1.upper()
+#If block dealing with almost everything, will most likely be moved to another file that will deal with classes instead
+def ifbm(mi, x, y):
+    if mi == "help" or mi == "h":
+        #Help Message
+        return "Your choices are currently Greater than or Less than, Sine, Csc, Cosine, Sec, Tangent, and Cotangent"
+    
+    elif mi == "sin" or mi == "sine":
+        #Does the sine calculation (Bad english, I know)
+        return sin(x) 
+    
+    elif mi == "csc" or mi =="cosecant":
+        #Does the csc calculation
+        return csc(x)
+    
+    elif mi == "cos" or mi == "cosine":
+        #Does the cosine calculation
+        return cos(x)
+    
+    elif mi == "sec" or mi == "secant":
+        #Does the sec calculation
+        return sec(x)
+    
+    elif mi == "tan" or mi == "tangent":
+        #Does the tangent calculation
+        return tan(x)
+    
+    elif mi == "cot" or mi == "cotangent":
+        return cot(x)
+    
+    elif mi == "gtorlt" or mi == "greater than or less than":
+        return gtorlt(x, y)
+    
+    else:
+        return "Something has gone horribly wrong, please restart the process"
+    
+#Creates the variable "output" which is basically another mediator between the top process and the print function
+output = ifbm(mi, i1, i2)
+
+#Simply prints the output
+print output
+
+
