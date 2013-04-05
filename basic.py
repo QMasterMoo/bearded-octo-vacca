d7e39947e3fa813e597765125929330a84cfc503
Moomasterq <cpsaicontact@gmail.com>
2013-04-04 22:30:45 -0400

    Added files needed in main.py
    Once again from the old plane trip files.

---
+++ basic.py
@@ -0,0 +1,19 @@
+#Created by Moomasterq
+#He has no idea what he is doing
+
+#Random Function
+def gtorlt(x, y):
+    if x > y:
+        return "X is greater than Y"
+    elif y > x:
+        return "Y is greater than X"
+    else:
+        return "These 2 numbers are equal"
+        
+#Function for telling you whether x or y exist
+def number_test(x):
+    if int(x) > 0:
+        return x
+    else:
+        return "Error"
+    
---
+++ trig.py
@@ -0,0 +1,27 @@
+#Imports math Package
+from math import sin, cos, tan
+
+#Deals with sine
+def sin(x):
+    return sin(x)
+
+#Deals with csc (No idea how to spell that)
+def csc(x):
+    return 1/sin(x)
+
+#Deals with cosine
+def cos(x):
+    return cos(x)
+
+#Deals with sec (Still no idea how to spell it)
+def sec(x):
+    return 1/cos(x)
+
+#Deals with tangent
+def tan(x):
+    return tan(x)
+
+#Deals with cotangent (I can spell this one)
+def cot(x):
+    return cot(x)
+
