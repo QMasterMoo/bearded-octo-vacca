081f93a2d57774ec746ba795ba1002c4002ddf5b
Moomasterq <cpsaicontact@gmail.com>
2013-04-04 22:23:12 -0400

    Added main file
    DOESN'T DO SHIT

---
+++ main.py
@@ -0,0 +1,13 @@
+#Created on 4/4/13 by Moomasterq
+#This file really does nothing
+
+#Takes input
+#This took me longer than I'd like to admit to figure out it was raw_input() not input()
+i1 = raw_input('Enter word: ')
+
+#Makes angry
+def angry(x):
+    return x.upper()
+
+#Prints input 1
+print i1.upper()
